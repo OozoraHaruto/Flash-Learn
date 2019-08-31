6dafb8592c783a85cfb580c85723998fea09ce74
Malcolm <chibi.taiyoozora@gmail.com>
2019-08-31 19:30:08 +0800

    removed lazy loading for clearer error debug

--- app/router/index.jsx
+++ app/router/index.jsx
@@ -7,19 +7,33 @@ import firebase from 'app/firebase';
 
 //Components
 // import Error404 from 'app/components/Errors/Error404';
-import Fallback from 'app/components/Fallback';
-const MainPage            = lazy(() => import(/* webpackChunkName: "cpo_MainPage" */ 'MainPage'));
-const ZWrapper            = lazy(() => import(/* webpackChunkName: "cpo_ZWrapper" */ 'ZWrapper'));
-const Credits             = lazy(() => import(/* webpackChunkName: "cpo_Credits" */ 'Credits'));
+// import Fallback from 'app/components/Fallback';
+// const MainPage            = lazy(() => import(/* webpackChunkName: "cpo_MainPage" */ 'MainPage'));
+// const ZWrapper            = lazy(() => import(/* webpackChunkName: "cpo_ZWrapper" */ 'ZWrapper'));
+// const Credits             = lazy(() => import(/* webpackChunkName: "cpo_Credits" */ 'Credits'));
 
-const AccLogin            = lazy(() => import(/* webpackChunkName: "cpo_Login" */ 'app/components/Account/Login'));
-const AccSignUp           = lazy(() => import(/* webpackChunkName: "cpo_SignUp" */ 'app/components/Account/SignUp'));
-const AccLogout           = lazy(() => import(/* webpackChunkName: "cpo_Logout" */ 'app/components/Account/Logout'));
+// const AccLogin            = lazy(() => import(/* webpackChunkName: "cpo_Login" */ 'app/components/Account/Login'));
+// const AccSignUp           = lazy(() => import(/* webpackChunkName: "cpo_SignUp" */ 'app/components/Account/SignUp'));
+// const AccLogout           = lazy(() => import(/* webpackChunkName: "cpo_Logout" */ 'app/components/Account/Logout'));
 // const AccProfile           = lazy(() => import(/* webpackChunkName: "cpo_Profile" */ 'app/components/Account/Profile'));
 
-const FAQMenu             = lazy(() => import(/* webpackChunkName: "cpo_FAQMenu" */ 'app/components/FAQ/index'));
-const FAQTests            = lazy(() => import(/* webpackChunkName: "cpo_FAQTests" */ 'app/components/FAQ/Tests'));
-const FAQGamification     = lazy(() => import(/* webpackChunkName: "cpo_FAQGamification" */ 'app/components/FAQ/Gamification'));
+// const FAQMenu             = lazy(() => import(/* webpackChunkName: "cpo_FAQMenu" */ 'app/components/FAQ/index'));
+// const FAQTests            = lazy(() => import(/* webpackChunkName: "cpo_FAQTests" */ 'app/components/FAQ/Tests'));
+// const FAQGamification     = lazy(() => import(/* webpackChunkName: "cpo_FAQGamification" */ 'app/components/FAQ/Gamification'));
+
+import Fallback from 'app/components/Fallback';
+
+import MainPage from 'MainPage';
+import ZWrapper from 'ZWrapper';
+import Credits from 'Credits';
+
+import AccLogin from 'app/components/Account/Login';
+import AccSignUp from 'app/components/Account/SignUp';
+import AccLogout from 'app/components/Account/Logout';
+
+import FAQMenu from 'app/components/FAQ/index';
+import FAQTests from 'app/components/FAQ/Tests';
+import FAQGamification from 'app/components/FAQ/Gamification';
 
 // Public - Route
 // Not Logged In - NoAuthRoute
