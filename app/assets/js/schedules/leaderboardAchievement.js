daef931dfdeb2b27145d33f485ed8158a9b14c9b
Malcolm <chibi.taiyoozora@gmail.com>
2019-10-26 18:22:17 +0800

    added calls for heroku schedules

--- app/assets/js/schedules/leaderboardAchievement.js
+++ app/assets/js/schedules/leaderboardAchievement.js
@@ -4,18 +4,18 @@ function calculatePointAchievement() {
   axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculatePointLeaderboardAchievements', {
     password: process.env.FIREBASE_FUNCTION_PASSWORD
   }).then(function (response) {
-    console.log("やった", response.data)
+    console.log("やった!ポイントの", response.data)
   }).catch(function (error) {
-    console.log("ダメだ", error);
+    console.log("ダメだ!ポイントの", error);
   })
 }
 function calculateTestAchievement() {
   axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculateTestLeaderboardsAchievements', {
     password: process.env.FIREBASE_FUNCTION_PASSWORD
   }).then(function (response) {
-    console.log("やった", response.data)
+    console.log("やった!テストの", response.data)
   }).catch(function (error) {
-    console.log("ダメだ", error);
+    console.log("ダメだ!テストの", error);
   })
 }
 calculatePointAchievement();
