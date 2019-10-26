40ec957724b31d7a994c6d7ce8b996596b214334
Malcolm <chibi.taiyoozora@gmail.com>
2019-10-26 12:15:58 +0800

    test function v4

--- app/assets/js/schedules/leaderboardAchievement.jsx
+++ app/assets/js/schedules/leaderboardAchievement.jsx
@@ -5,7 +5,7 @@ function sayHello() {
   axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculateLeaderboardAchievements', {
     password: process.env.FIREBASE_FUNCTION_PASSWORD
   }).then(function (response) {
-    console.log("やった", response.data());
+    console.log("やった", response.data)
   }).catch(function (error) {
     console.log("ダメだ", error);
   })
