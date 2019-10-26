0c163f904c19417aa5a9a100c1ccb5feef35d446
Malcolm <chibi.taiyoozora@gmail.com>
2019-10-26 18:53:24 +0800

    changed files for crons

---
+++ app/assets/js/cronJobs.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+var moment = require('moment');
+
+// Daily Jobs
+function calculatePointAchievement() {
+  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculatePointLeaderboardAchievements', {
+    password: process.env.FIREBASE_FUNCTION_PASSWORD
+  }).then(function (response) {
+    console.log("やった!ポイントの", response.data)
+  }).catch(function (error) {
+    console.log("ダメだ!ポイントの", error);
+  })
+}
+function calculateTestAchievement() {
+  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculateTestLeaderboardsAchievements', {
+    password: process.env.FIREBASE_FUNCTION_PASSWORD
+  }).then(function (response) {
+    console.log("やった!テストの", response.data)
+  }).catch(function (error) {
+    console.log("ダメだ!テストの", error);
+  })
+}
+
+
+// Monthly Jobs
+function resetLeaderboard() {
+  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/resetLeaderboards', {
+    password: process.env.FIREBASE_FUNCTION_PASSWORD
+  }).then(function (response) {
+    console.log("やった!", response.data)
+  }).catch(function (error) {
+    console.log("ダメだ!", error);
+  })
+}
+
+
+// Call Functions
+Promise.all([
+  calculatePointAchievement(),
+  calculateTestAchievement(),
+]).then(() =>{
+  const monthlyJobs = []
+  if (moment().endOf("month").diff(moment(), "days") == 0) {
+    monthlyJobs = [
+      resetLeaderboard(),
+    ]
+  }
+  return Promise.all(monthlyJobs)
+}).then(() =>{
+  console.log("すべて完了しました")
+}).catch(function (error) {
+  console.log("クッソ！", error);
+})
