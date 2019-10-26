1cca3d436f51a3d524d8507aeab0a70f61ae7656
Malcolm <chibi.taiyoozora@gmail.com>
2019-10-26 17:16:00 +0800

    test function v5

--- app/assets/js/schedules/leaderboardAchievement.jsx
+++ app/assets/js/schedules/leaderboardAchievement.jsx
@@ -1,8 +1,7 @@
 const axios = require('axios');
 
-function sayHello() {
-  console.log('こんにちは！');
-  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculateLeaderboardAchievements', {
+function calculatePointAchievement() {
+  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculatePointLeaderboardAchievements', {
     password: process.env.FIREBASE_FUNCTION_PASSWORD
   }).then(function (response) {
     console.log("やった", response.data)
@@ -10,4 +9,14 @@ function sayHello() {
     console.log("ダメだ", error);
   })
 }
-sayHello();
+function calculateTestAchievement() {
+  axios.post('https://us-central1-flashlearn-534b5.cloudfunctions.net/calculateTestLeaderboardsAchievements', {
+    password: process.env.FIREBASE_FUNCTION_PASSWORD
+  }).then(function (response) {
+    console.log("やった", response.data)
+  }).catch(function (error) {
+    console.log("ダメだ", error);
+  })
+}
+calculatePointAchievement();
+calculateTestAchievement();
